a772f5f09f41614c794621f7fb6f4e1269cbc506
Sachin Pant <sachinpant1326@gmail.com>
2020-05-18 09:17:42 +0530

    new update

--- Loan/src/main/java/com/cg/controller/LoanController.java
+++ Loan/src/main/java/com/cg/controller/LoanController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,10 +74,10 @@ public class LoanController {
 		return new ResponseEntity<Map<String,String>>(response,HttpStatus.OK);
 	}
 	
-	@GetMapping("/foreclose/{id}")
-	public String foreClose(@PathVariable String id) {
+	@DeleteMapping("/foreclose/{id}")
+	public ResponseEntity<String> foreClose(@PathVariable String id) {
 		bankService.foreCloseAccount(id);
-		return "done";
+		return new ResponseEntity<String>("foreclosed",HttpStatus.OK);
 	}
 	
 	@GetMapping("calculateemi/{amount}/{type}/{month}")
